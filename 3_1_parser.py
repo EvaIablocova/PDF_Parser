64a22ed670862de7ba54ceaf0f389332d485a8b0
Eva Iablocova <evaiablocova@gmail.com>
2025-07-30 16:25:06 +0300

    -2 pixels in parser

--- 3_1_parser.py
+++ 3_1_parser.py
@@ -67,6 +67,11 @@ def hyphenate_text(text, page, j, y, i, x, equal_columns_with_dash):
 
 def read_page(y, x, page, all_data, equal_columns_with_dash):
             for i in range(len(y) - 1):
+
+                    if i == 2 or i==11 or i==14 or i==16:
+                        print(1)
+
+
                     row_data = []
                     for j in range(len(x) - 1):
                         bbox = (x[j], y[i], x[j + 1], y[i + 1])
@@ -117,7 +122,8 @@ def parse_pdf(pdf_url, x, search_pattern, parsed_data_file_name, count_columns, 
                     text_line = " ".join(word['text'] for word in line_words)
                     other.append({'top': top, 'text': text_line})
 
-                md_tops = [entry['top'] - 1 for entry in other if md_pattern.search(entry['text'])]
+                md_tops = [entry['top'] - 2 for entry in other if md_pattern.search(entry['text'])]
+                # md_tops = [entry['top'] - 1 for entry in other if md_pattern.search(entry['text'])]
                 y = sorted(set(md_tops))
                 if y and y[-1] < page.height:
                     y.append(page.height - 40)
